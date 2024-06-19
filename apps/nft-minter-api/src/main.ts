68b3b3a72ab0beac0a3695dca4429b8fe375ff2d
Filipe Abreu <filabreu@gmail.com>
2024-06-18 21:38:03 -0300

    Add API endpoint to pin uploaded files to IPFS

--- apps/nft-minter-api/src/main.ts
+++ apps/nft-minter-api/src/main.ts
@@ -4,15 +4,40 @@
  */
 
 import express from 'express';
-import * as path from 'path';
+import dotenv from "dotenv"
+import { Readable } from 'stream';
+import cors from 'cors';
+import Multer from 'multer';
+import PinataClient from '@pinata/sdk';
+
+dotenv.config();
+
+const upload = Multer({
+  storage: Multer.memoryStorage(),
+  limits: {
+    fileSize: 5 * 1024 * 1024,
+  },
+});
 
 const app = express();
 
-app.use('/assets', express.static(path.join(__dirname, 'assets')));
+app.use(cors());
+app.use(express.json());
 
-app.get('/api', (req, res) => {
-  res.send({ message: 'Welcome to nft-minter-api!' });
-});
+app.post('/file', upload.single('file'), async (req, res) => {
+  const fileStream = Readable.from(req.file.buffer);
+  const pinata = new PinataClient({ pinataJWTKey: process.env.PINATA_JWT_KEY });
+
+  const pinataResult = await pinata.pinFileToIPFS(fileStream, {
+    pinataMetadata: {
+      name: req.file.originalname,
+    },
+  });
+
+  res.json({
+    fileUrl: `https://gateway.pinata.cloud/ipfs/${pinataResult.IpfsHash}`,
+  });
+})
 
 const port = process.env.PORT || 3333;
 const server = app.listen(port, () => {
